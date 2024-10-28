608150becf43e0061d7d80ae68ca0fe465e6b338
Vaibhav <vaibhav.arekar02@gmail.com>
2024-10-28 11:18:22 +0530

    updating order details

--- src/main/java/com/clinic/myclinic/model/MedicineWithAmount.java
+++ src/main/java/com/clinic/myclinic/model/MedicineWithAmount.java
@@ -3,15 +3,19 @@ package com.clinic.myclinic.model;
 public class MedicineWithAmount {
 	private String item;
 	private int amount;
+	private int qty;
+	private int price;
 	
 	public MedicineWithAmount() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public MedicineWithAmount(String item, int amount) {
+	public MedicineWithAmount(String item, int amount, int qty, int price) {
 		super();
 		this.item = item;
 		this.amount = amount;
+		this.qty = qty;
+		this.price = price;
 	}
 	
 	public String getItem() {
@@ -25,5 +29,17 @@ public class MedicineWithAmount {
 	}
 	public void setAmount(int amount) {
 		this.amount = amount;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
 	}
 }
