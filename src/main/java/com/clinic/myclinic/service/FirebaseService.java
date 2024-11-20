cc002c97f0fc720ad2e7af39959f597bd4fded98
Vaibhav <vaibhav.arekar02@gmail.com>
2024-11-20 21:41:43 +0530

    updating total order count

--- src/main/java/com/clinic/myclinic/service/FirebaseService.java
+++ src/main/java/com/clinic/myclinic/service/FirebaseService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.clinic.myclinic.bean.AboutUiBeans;
 import com.clinic.myclinic.bean.HomeUiBeans;
+import com.clinic.myclinic.dao.FirebaseHomeDAO;
 import com.clinic.myclinic.dao.FirebaseUIDAO;
 import com.google.cloud.Timestamp;
 
@@ -17,10 +18,21 @@ public class FirebaseService {
 	
 	@Autowired
 	FirebaseUIDAO firebaseDAO;
+	@Autowired
+	FirebaseHomeDAO firebaseHomeDAO;
 
 	@SuppressWarnings("unchecked")
 	public Map<String, Object> getUiDetails(String contentIn) throws ExecutionException, InterruptedException {
 		Map<String, Object> textDetails = firebaseDAO.getUiDetails(contentIn);
+		Map<String, Object> homeDetails = (Map<String, Object>) textDetails.get("home");
+		String cureCount = (String) homeDetails.get("cure_count");
+		long orderCount = firebaseHomeDAO.getOrders().entrySet().stream()
+			.filter(entry -> {
+				Map<String, Object> orders = (Map<String, Object>) entry.getValue();
+				return !orders.get("status").equals("D");
+			}).count();
+		homeDetails.put("cure_count", cureCount.replace("$count", countStandardization(orderCount)));
+		textDetails.put("home", homeDetails);
 		
 		Map<Integer, Object> menu = new HashMap<Integer, Object>();
 		Map<String, Object> menuDetails = firebaseDAO.getHeaderDetails("user");
@@ -52,4 +64,17 @@ public class FirebaseService {
 		return firebaseDAO.storeUiDetails(contentIn, aboutUiBeans, "about");
 	}
 	
+	private String countStandardization(long value) {
+		if (value<10) {
+			return String.valueOf(value);
+		} else if (value<1000) {
+			return (value/10)*10+"+";
+		} else if (value<1000000) {
+			return value/1000+"K+";
+		} else if (value<1000000000) {
+			return value/1000000+"M+";
+		} else {
+			return value/1000000000+"B+";
+		}
+	}
 }
